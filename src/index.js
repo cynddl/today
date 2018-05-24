83979332ad2973e8ddcd33224a50c28da2021a73
Luc Rocher <luc@rocher.lc>
2018-05-24 15:43:25 +0200

    Replace react-scripts with parcel

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
 registerServiceWorker();
